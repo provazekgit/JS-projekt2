8f3e66fa1332435b24d06d89c3fbc4ed1fd9092d
provazekgit <165811565+provazekgit@users.noreply.github.com>
2025-01-25 23:59:11 +0100

    Add files via upload

---
+++ varianta-popi/script.js
@@ -0,0 +1,19 @@
+const menuIcon = document.querySelector(".menu-icon")
+const menuList = document.querySelector("nav")
+const hamburgerIcon = document.querySelector(".fa-solid")
+
+// menuIcon.addEventListener("click", () => {
+//     console.log("Bylo kliknuto")
+// })
+
+menuIcon.addEventListener("click", () => {
+    if (hamburgerIcon.classList[1] === "fa-bars") {
+        hamburgerIcon.classList.add("fa-xmark")
+        hamburgerIcon.classList.remove("fa-bars")
+        menuList.style.display = "block"  // menu
+    } else {
+        hamburgerIcon.classList.add("fa-bars")
+        hamburgerIcon.classList.remove("fa-xmark")
+        menuList.style.display = "none"  // menu
+    }
+})
